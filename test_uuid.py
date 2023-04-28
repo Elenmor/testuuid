3781b11b02a20b76aff89ccf5df8f334eacbfe04
Elena Morilova <elena.morilova@mail.ru>
2023-04-28 17:25:13 +0400

    only one variant

---
+++ test_uuid.py
@@ -0,0 +1,23 @@
+import uuid
+
+
+def test_change():
+
+   identificator = uuid.uuid4().hex
+   #print(identificator)
+
+   print('autotest' + identificator[9:36])
+
+
+   #splituid=("autotest", identificator[0:8])
+   #print(splituid)
+
+
+
+   #склеить
+   #print(identificator)
+
+
+#сечение
+
+#сплит
